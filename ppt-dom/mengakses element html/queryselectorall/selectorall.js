9f9d88ea48b98b97ad297835e3dedf7677d436c5
Rasya satria <rashyyjohan@gmail.com>
2025-05-12 13:49:23 +0700

    Update selectorall.js

--- ppt-dom/mengakses element html/queryselectorall/selectorall.js
+++ ppt-dom/mengakses element html/queryselectorall/selectorall.js
@@ -1,7 +1,6 @@
 function ubahsemua() {
     let semuakotak = document.querySelectorAll(".kotak");
     semuakotak.forEach((kotak) => {
-        
         kotak.style.backgroundColor = "lightblue";
         kotak.style.padding = "10px";
         kotak.style.margin = "5px";
